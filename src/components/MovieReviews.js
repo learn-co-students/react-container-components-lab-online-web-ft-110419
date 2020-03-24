5447e6278f2a9117ad98da7e8de91fcd13a0737d
BrandonMWeaver <a7xncob@gmail.com>
2020-03-24 17:23:42 -0400

    Done.

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,21 @@
-// Code MovieReviews Here
+import React from 'react';
+
+const MovieReviews = props => {
+	const reviews = props.reviews.map((review, index) => {
+		return (
+			<div key={index} className="review" style={{margin: "5px", padding: "5px", borderRadius: "5px", backgroundColor: "#ccc"}}>
+				<h3 style={{margin: "0"}}>{review.display_title}</h3>
+				<h5>Reviewed by: {review.byline}</h5>
+				<i>"{review.summary_short}"</i>
+			</div>
+		)
+	});
+
+	return (
+		<div className="review-list">
+			{reviews}
+		</div>
+	)
+}
+
+export default MovieReviews;
--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -6,4 +6,38 @@ const NYT_API_KEY = 'dGpQ5OmGP2SgfvZimlpCUoF4iOag9qzZ';
 const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/search.json?'
             + `api-key=${NYT_API_KEY}`;
 
-// Code SearchableMovieReviewsContainer Here
+export default class SearchableMovieReviewsContainer extends Component {
+
+  state = { reviews: [], searchTerm: '' }
+
+  handleSubmit = (event, query) => {
+    event.preventDefault();
+    console.log(query)
+    fetch(`${URL}&query=${query}`)
+    .then(response => response.json())
+    .then(object => {
+      this.setState({
+        reviews: object.results
+      });
+    });
+  }
+
+  handleChange = event => {
+    this.setState({
+      searchTerm: event.target.value
+    });
+  }
+
+  render() {
+    return (
+      <div className="searchable-movie-reviews">
+        <form onSubmit={(event) => this.handleSubmit(event, this.state.searchTerm)}>
+          <input onChange={this.handleChange} id="query" type="text" />
+          <input type="submit" value="Search" />
+        </form>
+        {this.state.reviews.length > 0 ? <h3>Search Results:</h3> : ''}
+        <MovieReviews reviews={this.state.reviews} />
+      </div>
+    );
+  }
+}
