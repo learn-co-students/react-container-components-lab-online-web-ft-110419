2d2142521a2330f4ccc8273daa12ccabd8c65c29
cejimenez0123 <christianjimenez0123@gmail.com>
2020-03-27 10:24:57 -0400

    Done.

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,14 @@
 // Code MovieReviews Here
+import React from 'react'
+import { render } from 'enzyme'
+
+const MovieReviews = ( props) => { 
+
+
+    const helperFuncs = {
+        list: () => props.reviews.map(rev => <div key={rev.headline} className="review">{rev.headline}</div>)
+      };
+    
+      return <div className="review-list">{helperFuncs.list()}</div>;
+};
+export default  MovieReviews 
